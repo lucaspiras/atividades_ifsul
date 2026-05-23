461580cd712ce5606d1ac4cbb77f22638ffc2205
unknown <piras314@gmail.com>
2026-05-23 15:11:43 -0300

    Ajustando o gabarito

--- correcoes/corretor.js
+++ correcoes/corretor.js
@@ -41,10 +41,10 @@ async function supabaseFetch(path, method, body, prefer = 'return=representation
 
 /** Gabarito oficial da prova */
 const GABARITO = {
-  1:'B', 2:'A', 3:'B', 4:'C', 5:'C',
-  6:'B', 7:'B', 8:'B', 9:'B', 10:'B',
-  11:'B', 12:'C', 13:'D', 14:'C', 15:'B',
-  16:'C', 17:'B', 18:'A', 19:'D', 20:'B'
+  1:'E', 2:'A', 3:'B', 4:'B', 5:'C',
+  6:'A', 7:'D', 8:'E', 9:'A', 10:'B',
+  11:'C', 12:'C', 13:'D', 14:'C', 15:'D',
+  16:'E', 17:'E', 18:'A', 19:'D', 20:'B'
 };
 
 /** Alternativas possíveis */
